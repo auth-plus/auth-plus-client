2ed94e013b4611e10045727a23b190aa2d5b5ee1
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2023-01-23 01:27:20 -0300

    Feature/login page style (#5)

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,55 +1,27 @@
 <script>
-	import { credential } from '../../stores/auth';
+	import Choose from './choose.svelte';
+	import Code from './code.svelte';
+	import Default from './default.svelte';
 
-	let email = '';
-	let password = '';
-	let strategyOption = null;
-	let code = '';
 	let mfaChoose = null;
 	let mfaCodeHash = null;
-	async function submit() {
-		const mfaCh = await credential.login('admin@authplus.com', '7061651770d7b3ad8fa96e7a8bc61447');
-		if (mfaCh != null) {
-			mfaChoose = mfaCh;
-		}
-	}
 
-	async function submitChoose() {
-		mfaCodeHash = await credential.chooseStrategy(mfaChoose.hash, strategyOption);
+	function setMfaChoose(value) {
+		mfaChoose = value;
 	}
-
-	async function submitCode() {
-		await credential.loginCode(mfaCodeHash, code);
-		mfaCode = true;
+	function setMfaCodeHash(value) {
+		mfaCodeHash = value;
 	}
 </script>
 
-<form on:submit={submit}>
-	<label for="login-email">Email</label>
-	<input id="login-email" bind:value={email} placeholder="enter your email" />
-	<label for="login-pw">Password</label>
-	<input id="login-pw" bind:value={password} placeholder="enter your password" />
-	<input type="submit" value="Login" />
-</form>
-<a href="/forget">Esqueci a senha</a>
-
-{#if mfaChoose != null}
-	<form on:submit={submitChoose}>
-		<label for="login-strategy">Choose One</label>
-		<select name="cars" id="login-strategy" bind:value={strategyOption}>
-			<option value={null}>-</option>
-			{#each mfaChoose.strategyList as strate}
-				<option value={strate}>{strate}</option>
-			{/each}
-		</select>
-		<input type="submit" value="Choose" />
-	</form>
+{#if mfaChoose == null && mfaCodeHash == null}
+	<Default {setMfaChoose} />
 {/if}
 
-{#if mfaCodeHash !== null}
-	<form on:submit={submitCode}>
-		<label for="login-code">Code</label>
-		<input id="login-code" bind:value={code} placeholder="123456" />
-		<input type="submit" value="Login" />
-	</form>
+{#if mfaChoose != null && mfaCodeHash == null}
+	<Choose hash={mfaChoose.hash} list={mfaChoose.strategyList} {setMfaCodeHash} />
+{/if}
+
+{#if mfaCodeHash != null}
+	<Code hash={mfaCodeHash} />
 {/if}
---
+++ src/routes/login/code.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { credential } from '../../stores/auth';
+
+	export let hash;
+
+	let code = '';
+
+	async function submitCode() {
+		await credential.loginCode(hash, code);
+		mfaCode = true;
+	}
+</script>
+
+<form on:submit={submitCode}>
+	<label for="login-code">Code</label>
+	<input id="login-code" bind:value={code} placeholder="123456" />
+	<input type="submit" value="Login" />
+</form>
+
+<style lang="scss">
+	form {
+		background-color: cornflowerblue;
+		padding: 2vw;
+		border-radius: 1vw;
+		color: white;
+	}
+</style>
