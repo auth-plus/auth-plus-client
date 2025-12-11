bf426510a5ae33b580fcd004f0e6d35c6e8eefed
AndrewHanasiro <andrewkhanasiro@gmail.com>
2025-12-11 17:28:27 -0300

    fix: test

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -2,5 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.locator('h1')).toBeVisible()
+	const navElement = page.locator('nav');
+	await expect(navElement).toBeVisible()
+	await expect(navElement).toContainText('Auth +');
 })
