ef2a57e818351ecfdd1fe8cd4527356b2d315b48
AndrewHanasiro <andrewkhanasiro@gmail.com>
2025-12-17 17:16:47 -0300

    better use of test

--- test/Header.svelte.test.ts
+++ test/Header.svelte.test.ts
@@ -6,11 +6,6 @@ import Modal from '../src/routes/Header.svelte'
 describe('Header.svelte', () => {
 	test('should render Auth+', () => {
 		render(Modal)
-		// `queryByRole` is used because it returns null if not found, unlike `getByRole` which throws an error.
-		const nav = screen.queryAllByText('Auth +')
-		if (nav.length != 1) {
-			throw new Error('Should have only one')
-		}
-		expect(nav[0]).toBeInTheDocument()
+		expect(screen.getByText('Auth +')).toBeInTheDocument()
 	})
 })
